08c4be3b1285ef872f5ec47363c87a13a05cc709
roryTheGnome <s29991@pjwstk.edu.pl>
2025-05-12 10:34:33 +0200

    now the shell is ready

--- APBD_Test1/Controllers/AppointmentController.cs
+++ APBD_Test1/Controllers/AppointmentController.cs
@@ -1,6 +1,34 @@
-﻿namespace APBD_Test1.Controllers;
+﻿using APBD_Test1.Models;
+using Microsoft.AspNetCore.Mvc;
 
-public class AppointmentController
+namespace APBD_Test1.Controllers;
+
+[ApiController]
+[Route("api/appointments")]
+public class AppointmentController:ControllerBase
 {
+    private readonly IConfiguration _configuration;
+
+    public AppointmentController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+
+    [HttpGet]
+    [Route("api/appointments/{id}")]
+    public async Task<List<Appointment>> GetAppointmentInformation(string id)
+    {
+        return Ok();
+    }
+
+
+
+    //TODO : the second end point to implemant later
+    [HttpPost]
+    public async Task<List<Appointment>> AddAppointment(Appointment appointment)
+    {
+        return Ok();
+    }
     
 }
